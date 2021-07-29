d8051cb6fed7709984e003b3eb1c124d275a5b39
BorisLechev <b.k.lechev@gmail.com>
2021-07-29 10:02:15 +0300

    Add Simple Factory

---
+++ CreationalPatterns/SimpleFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffeeFactory = new CoffeeFactory();
+            var cappuccino = coffeeFactory.GetCoffee(CoffeeType.Cappuccino);
+            var regularCoffee = coffeeFactory.GetCoffee(CoffeeType.Regular);
+
+            Console.WriteLine($"Cappuccino - Milk Content: {cappuccino.MilkContent} ml, Coffee Content: {cappuccino.CoffeeContent} ml.");
+            Console.WriteLine($"Regular coffee - Milk Content: {regularCoffee.MilkContent} ml, Coffee Content: {regularCoffee.CoffeeContent} ml.");
+        }
+    }
+}
