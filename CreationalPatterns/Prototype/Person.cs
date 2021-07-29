d8705eebe2e45f335fc01b7cdaa2a9071ceb5c4c
BorisLechev <b.k.lechev@gmail.com>
2021-07-29 21:07:51 +0300

    Prototype Pattern

---
+++ CreationalPatterns/Prototype/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return this.MemberwiseClone() as Person;
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = this.MemberwiseClone() as Person;
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = string.Copy(this.Name);
+            return clone;
+        }
+    }
+}
