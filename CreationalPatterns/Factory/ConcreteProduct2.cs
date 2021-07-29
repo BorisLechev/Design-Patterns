c7ff876c77d179b7e20e216ff2c167edd4a231c7
BorisLechev <b.k.lechev@gmail.com>
2021-07-29 16:38:37 +0300

    Factory Method pattern

---
+++ CreationalPatterns/Factory/ConcreteProduct2.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMethod
+{
+    // Concrete Products provide various implementations of the Product
+    // interface.
+    public class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "[Result of ConcreteProduct2]";
+        }
+    }
+}
